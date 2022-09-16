7dad04b592da2ee99adbf5c552db3ae6f819ce3f
Iliyan Jivraj <me@iliyan.dev>
2022-09-16 12:22:50 -0500

    bump to 0.0.3-dev:wq

--- Lab02/Part A/Q2 TryVariables/src/TryVariables.java
+++ Lab02/Part A/Q2 TryVariables/src/TryVariables.java
@@ -5,7 +5,7 @@ public class TryVariables {
         int testGrade = 100;
         long cityPopulation = 425612340;
         byte ageInYears = 19;
-        float  salesTax = .05F;		// the trailing F makes a float numeric literal
+        float  salesTax = .05F;        // the trailing F makes a float numeric literal
         double interestRate = 0.725;
         double avogadroNumber = 602214076000000000000000.;
         char finalGrade = 'A';
